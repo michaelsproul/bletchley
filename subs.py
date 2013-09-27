86324dba56f7bd58a36cd2b528f9b7bb8087ef24
Michael Sproul <micsproul@gmail.com>
2013-09-27 22:15:43 +1000

    Vigenere ciphers

--- subs.py
+++ subs.py
@@ -1,4 +1,4 @@
-"""Useful tools for solving substitution ciphers"""
+"""Useful tools for solving substitution ciphers."""
 
 import random
 
@@ -8,7 +8,7 @@ letters = [l for l in lc_letters]
 letters.extend(uc_letters)
 
 def caesar(text, shift=0):
-	"Perform a Caesar shift on a given piece of text"
+	"Perform a Caesar shift on a given piece of text."
 	shifted = ""
 	for c in text:
 		if c not in letters:
@@ -25,7 +25,7 @@ def caesar(text, shift=0):
 	return shifted
 
 def mono(text, key):
-	"Substitute each letter from the text with a match from the key"
+	"Substitute each letter from the text with a match from the key."
 	output = ""
 	for c in text:
 		if c in key:
@@ -35,7 +35,7 @@ def mono(text, key):
 	return output
 
 def random_alphabet():
-	"Generate a random key"
+	"Generate a random key."
 	shuffled = [i for i in range(26)]
 	for i in shuffled:
 		j = random.randint(0, 25)
@@ -45,26 +45,8 @@ def random_alphabet():
 	key = {uc_letters[i]: lc_letters[shuffled[i]] for i in shuffled}
 	return key
 
-def find_repeats(string, l=3):
-	"Find the repeated words of length l in a given string"
-	words = {}
-	n = len(string)
-	for i in range(0, n - l + 1):
-		w = string[i:i + l]
-		if w in words:
-			words[w] += 1
-		else:
-			words[w] = 1
-
-	repeats = {}
-	for w in words:
-		if words[w] > 1:
-			repeats[w] = words[w]
-
-	return repeats
-
 def freq(ciphertext):
-	"Count the frequency of each letter in the ciphertext"
+	"Count the frequency of each letter in the ciphertext."
 	freq = {l: 0 for l in uc_letters}
 	n = 0
 	for c in ciphertext:
@@ -75,3 +57,51 @@ def freq(ciphertext):
 	for l in freq:
 		freq[l] = round(freq[l]/n, 3)
 	return freq
+
+def find_repeats(string, l=3):
+	"""Find the repeated words of length l in a given string.
+
+	Return a list of the repeated words found and the positions at which
+	they appear in the string.
+	"""
+	words = {}
+	n = len(string)
+	for i in range(0, n - l + 1):
+		w = string[i:i + l]
+		if w in words:
+			words[w].append(i)
+		else:
+			words[w] = [i]
+
+	repeats = {}
+	for w in words:
+		if len(words[w]) > 1:
+			repeats[w] = words[w]
+
+	return repeats
+
+def gap_distances(repeats):
+	"Calculate the set of distances between repeated words."
+	distances = set()
+	for w in repeats:
+		pos = repeats[w]
+		for i in range(len(pos) - 1):
+			distances.add(pos[i + 1] - pos[i])
+	return distances
+
+def vigenere(text, key):
+	"Perform a Vigenere cipher on the given text using the key provided."
+	output = ""
+
+	# Calculate the equivalent caesar shift for each letter in the key
+	key = key.lower()
+	shifts = []
+	for l in key:
+		shifts.append(lc_letters.index(l))
+
+	# Encipher each letter according the key shifts
+	n = len(key)
+	for i in range(len(text)):
+		output += caesar(text[i], shifts[i % n])
+
+	return output
